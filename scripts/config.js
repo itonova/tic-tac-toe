d83339003c9662ac001e5df4af066c8c6875040d
itonova <ivaneta.to@gmail.com>
2022-02-17 19:45:19 +0200

    Initial commit

---
+++ scripts/config.js
@@ -0,0 +1,40 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid;
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+  document.body.style.overflow = 'hidden';
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.reset();
+  document.body.style.overflow = 'auto';
+}
+
+function changedPlayernameInput() {
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name.";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
+}
